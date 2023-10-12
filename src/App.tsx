126ff1ec46f8b75adeaa5c6440b27a2d6ab4b69d
Brandon Marafino <Marafino@udel.edu>
2023-10-12 18:45:08 -0400

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import "./App.css";
 import { Quizzer } from "./quizzer/Quizzer";
 import { Button } from "react-bootstrap";
 //import { ShowHideTasks } from "./tasks/ShowHideTasks";
-//comment for deploy
-//new code
+//comment for deploy try again
 function App(): JSX.Element {
     const [quizzer, setQuizzer] = useState<boolean>(true);
 
